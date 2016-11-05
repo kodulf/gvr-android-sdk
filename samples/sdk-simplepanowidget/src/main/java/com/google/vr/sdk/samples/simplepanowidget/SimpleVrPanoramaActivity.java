8011785454b2a25d670a2bb4789dbd938c672c2a
章银珊 <lao_xue@hotmail.com>
2016-11-05 21:23:21 +0800

    update

--- samples/sdk-simplepanowidget/src/main/java/com/google/vr/sdk/samples/simplepanowidget/SimpleVrPanoramaActivity.java
+++ samples/sdk-simplepanowidget/src/main/java/com/google/vr/sdk/samples/simplepanowidget/SimpleVrPanoramaActivity.java
@@ -27,6 +27,7 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.util.Pair;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.vr.sdk.widgets.pano.VrPanoramaEventListener;
@@ -62,6 +63,7 @@ public class SimpleVrPanoramaActivity extends Activity {
   /** Configuration information for the panorama. **/
   private Options panoOptions = new Options();
   private ImageLoaderTask backgroundImageLoaderTask;
+  private boolean doubleTime;
 
   /**
    * Called when the app is launched via the app icon or an intent using the adb command above. This
@@ -79,6 +81,13 @@ public class SimpleVrPanoramaActivity extends Activity {
 
     panoWidgetView = (VrPanoramaView) findViewById(R.id.pano_view);
     panoWidgetView.setEventListener(new ActivityEventListener());
+    panoWidgetView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"clicked" +
+                "",Toast.LENGTH_LONG).show();
+      }
+    });
 
     // Initial launch of the app or an Activity recreation due to rotation.
     handleIntent(getIntent());
@@ -127,8 +136,8 @@ public class SimpleVrPanoramaActivity extends Activity {
       // Cancel any task from a previous intent sent to this activity.
       backgroundImageLoaderTask.cancel(true);
     }
-    backgroundImageLoaderTask = new ImageLoaderTask();
-    backgroundImageLoaderTask.execute(Pair.create(fileUri, panoOptions));
+//    backgroundImageLoaderTask = new ImageLoaderTask();
+//    backgroundImageLoaderTask.execute(Pair.create(fileUri, panoOptions));
   }
 
   @Override
@@ -156,20 +165,84 @@ public class SimpleVrPanoramaActivity extends Activity {
     super.onDestroy();
   }
 
+  public void changeImage(View view) {
+    if(!doubleTime) {
+      backgroundImageLoaderTask = null;
+
+      Toast.makeText(getApplicationContext(), "clicked" +
+              "", Toast.LENGTH_LONG).show();
+
+      //Uri uri = Uri.parse("file:///android_asset/demo-converted.jpg");
+      Uri uri = Uri.parse("file:///android_asset/andes.jpg");
+
+      fileUri = uri;
+      backgroundImageLoaderTask = new ImageLoaderTask();
+//    Uri.parse("android.resource://" + getApplicationContext().getPackageName() + "/" + R.assets.test);
+
+      AssetManager assetManager = getAssets();
+      InputStream inputStream = null;
+      try {
+        inputStream = assetManager.open("andes.jpg");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      backgroundImageLoaderTask.execute(Pair.create(inputStream, panoOptions));
+      doubleTime = true;
+    }else{
+      backgroundImageLoaderTask = null;
+      AssetManager assetManager = getAssets();
+      InputStream inputStream = null;
+      Toast.makeText(getApplicationContext(), "clicked double" +
+              "", Toast.LENGTH_LONG).show();
+      try {
+        inputStream = assetManager.open("demo-converted.jpg");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      backgroundImageLoaderTask = new ImageLoaderTask();
+      backgroundImageLoaderTask.execute(Pair.create(inputStream, panoOptions));
+      doubleTime = false;
+    }
+  }
+
   /**
    * Helper class to manage threading.
    */
-  class ImageLoaderTask extends AsyncTask<Pair<Uri, Options>, Void, Boolean> {
+  class ImageLoaderTask extends AsyncTask<Pair<InputStream, Options>, Void, Boolean> {
 
     /**
      * Reads the bitmap from disk in the background and waits until it's loaded by pano widget.
      */
     @Override
-    protected Boolean doInBackground(Pair<Uri, Options>... fileInformation) {
+//    protected Boolean doInBackground(Pair<Uri, Options>... fileInformation) {
+//      Options panoOptions = null;  // It's safe to use null VrPanoramaView.Options.
+//      InputStream istr = null;
+//      if (fileInformation == null || fileInformation.length < 1
+//          || fileInformation[0] == null || fileInformation[0].first == null) {
+//        AssetManager assetManager = getAssets();
+//        try {
+//          istr = assetManager.open("andes.jpg");
+//          panoOptions = new Options();
+//          panoOptions.inputType = Options.TYPE_STEREO_OVER_UNDER;
+//        } catch (IOException e) {
+//          Log.e(TAG, "Could not decode default bitmap: " + e);
+//          return false;
+//        }
+//      } else {
+//        try {
+//          istr = new FileInputStream(new File(fileInformation[0].first.getPath()));
+//          panoOptions = fileInformation[0].second;
+//        } catch (IOException e) {
+//          Log.e(TAG, "Could not load file: " + e);
+//          return false;
+//        }
+//      }
+
+    protected Boolean doInBackground(Pair<InputStream, Options>... fileInformation) {
       Options panoOptions = null;  // It's safe to use null VrPanoramaView.Options.
       InputStream istr = null;
       if (fileInformation == null || fileInformation.length < 1
-          || fileInformation[0] == null || fileInformation[0].first == null) {
+              || fileInformation[0] == null || fileInformation[0].first == null) {
         AssetManager assetManager = getAssets();
         try {
           istr = assetManager.open("andes.jpg");
@@ -180,15 +253,10 @@ public class SimpleVrPanoramaActivity extends Activity {
           return false;
         }
       } else {
-        try {
-          istr = new FileInputStream(new File(fileInformation[0].first.getPath()));
+//          istr = new FileInputStream(new File(fileInformation[0].first.getPath()));
+          istr = fileInformation[0].first;
           panoOptions = fileInformation[0].second;
-        } catch (IOException e) {
-          Log.e(TAG, "Could not load file: " + e);
-          return false;
-        }
       }
-
       panoWidgetView.loadImageFromBitmap(BitmapFactory.decodeStream(istr), panoOptions);
       try {
         istr.close();
